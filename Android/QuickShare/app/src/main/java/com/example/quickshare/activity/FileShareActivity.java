614c6b387b3e4fe7ec04aa55ea15313cba0c6b7b
tarique <tarique.khan@inscripts.com>
2016-07-11 18:15:47 +0530

    File Sharing for android started

---
+++ Android/QuickShare/app/src/main/java/com/example/quickshare/activity/FileShareActivity.java
@@ -0,0 +1,15 @@
+package com.example.quickshare.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.quickshare.R;
+
+public class FileShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_share);
+    }
+}
